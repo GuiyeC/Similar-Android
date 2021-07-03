5ec44e549a825b25813c89a3287fd72737785122
Guillermo Cique Fernández <GuiyeC@gmail.com>
2021-07-03 18:49:38 +0200

    Allow custom Response

--- similar/src/main/java/com/guiyec/similar/Response.kt
+++ similar/src/main/java/com/guiyec/similar/Response.kt
@@ -1,5 +1,6 @@
 package com.guiyec.similar
 
+import java.io.ByteArrayInputStream
 import java.io.InputStream
 
 data class Response(
@@ -7,12 +8,35 @@ data class Response(
     val statusCode: Int,
     val headers: Map<String, String>
 ) {
-    data class Data(private val response: okhttp3.Response) {
-        val inputStream: InputStream get() = response.body!!.byteStream()
-        val bytes: ByteArray get() = response.body!!.bytes()
-        val string: String get() = response.body!!.string()
+    interface Data {
+        val inputStream: InputStream
+        val bytes: ByteArray
+        val string: String
     }
 
     constructor(response: okhttp3.Response) :
-            this(Data(response), response.code, response.headers.toMap())
+            this(OkhttpData(response), response.code, response.headers.toMap())
+}
+
+data class ResponseData(override val bytes: ByteArray): Response.Data {
+    override val inputStream: InputStream get() = ByteArrayInputStream(bytes)
+    override val string: String get() = bytes.toString(Charsets.UTF_8)
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+        other as ResponseData
+        if (!bytes.contentEquals(other.bytes)) return false
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return bytes.contentHashCode()
+    }
+}
+
+data class OkhttpData(private val response: okhttp3.Response): Response.Data {
+    override val inputStream: InputStream get() = response.body!!.byteStream()
+    override val bytes: ByteArray get() = response.body!!.bytes()
+    override val string: String get() = response.body!!.string()
 }
